582187821034c462d242ee2393fe035e259f5aa5
Kalle Sova <klsova@utu.fi>
2025-01-15 12:59:42 +0200

    1.1 done

---
+++ osa1/kursstiedot/src/App.jsx
@@ -0,0 +1,40 @@
+const App = () => {
+  const Header = (props) => {
+    console.log(props)
+    return (
+      <div>
+        <h1>{props.course}</h1>
+      </div>
+    )
+  }
+
+  const Content = (props) => {
+    console.log(props)
+    return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+    )
+  }
+
+  const Total = (props) => {
+    console.log(props)
+    return (
+      <p>
+        {props.total} {props.exercises}
+      </p>
+    )
+  }
+
+  return (
+    <div>
+      <Header course ='Half Stack application development'/>
+      <Content part = 'Fundamentals of React' exercises = {10}/>
+      <Content part = 'Using props to pass data' exercises = {7}/>
+      <Content part = 'State of a component' exercises = {14}/>
+      <Total total = 'Number of exerises' exercises = {21}/>
+      </div>
+  )
+}
+
+export default App
