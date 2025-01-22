c82546c775f16fdd01a2e3045ea96b3238b75f89
Kalle Sova <klsova@utu.fi>
2025-01-22 15:18:00 +0200

    1.6 done

---
+++ osa1/palaute/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <body>
+        <h1> Give feedback </h1>
+        <button onClick={handleGoodClick}>Good</button>
+        <button onClick={handleNeutralClick}>Neutral</button>
+        <button onClick={handleBadClick}>Bad</button>
+        <h2> Statistics </h2>
+        <div>Good {good}</div>
+        <div>Neutral {neutral}</div>
+        <div>Bad {bad}</div>
+      </body>
+    </div>
+  )
+}
+
+export default App
