7f4df12b151619aa35edb0903f032860f5ee4185
Kalle Sova <klsova@utu.fi>
2025-02-03 15:22:12 +0200

    2.2 done

--- osa2/kurssitiedot/src/App2.1.jsx
+++ osa2/kurssitiedot/src/App2.1.jsx
@@ -45,14 +45,11 @@ const Header = ({name}) => {
 }
 
 const Content = ({parts}) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  
   return (
     <div>
       {parts.map(part => (
         <Part key={part.id} part={part} />
       ))}
-      <p> Total of {total} exercises</p>
     </div>
   )
 }
